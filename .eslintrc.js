c5f82d944ede6dedd95ad5481af077b86b165194
Richard Jedlička <jedlicka.r@gmail.com>
2022-10-05 20:24:33 +0200

    update eslint to not require importing react for jsx to work

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,13 @@
 module.exports = {
 	"env": {
+		"node": true,
 		"browser": true,
 		"es2021": true
 	},
 	"extends": [
 		"eslint:recommended",
 		"plugin:react/recommended",
+		"plugin:react/jsx-runtime",
 		"plugin:@typescript-eslint/recommended"
 	],
 	"overrides": [
