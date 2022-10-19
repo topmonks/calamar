6b6b79f295d835e516ac83a8679705b169e69b56
Richard Jedlička <jedlicka.r@gmail.com>
2022-10-19 10:03:16 +0200

    tests: test change

--- test/cypress/e2e/extrinsic.cy.ts
+++ test/cypress/e2e/extrinsic.cy.ts
@@ -2,7 +2,7 @@ describe("Extrinsic detail page", () => {
 	beforeEach(() => {
 		cy.clearCookies();
 		cy.visit("/kusama/extrinsic/0014932897-000002-93378");
-		cy.wait(1000); // TODO find out better waiting until page is loaded
+		cy.wait(1001); // TODO find out better waiting until page is loaded
 	});
 
 	it("shows extrinsic detail page", () => {
