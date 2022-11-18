37d340e8a7ca9b8e9194a5fd2cf52da07b8fde06
Richard Jedlička <jedlicka.r@gmail.com>
2022-11-18 17:57:27 +0100

    fix shortenId

--- src/utils/shortenId.ts
+++ src/utils/shortenId.ts
@@ -1,4 +1,3 @@
 export const shortenId = (id: string) => {
-	return id.split("-").slice(0, -1).map(it => it.replace(/^0+([^0]+)$/, "$1")).join("-");
-	return id.replace(/(^|-)0+([^-])/g, "$1$2").replace(/-[^-]*$/, "");
+	return id.split("-").slice(0, -1).map(it => it.replace(/^0+([^-])/, "$1")).join("-");
 };
