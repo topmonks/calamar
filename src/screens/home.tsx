b349b456a820af813037ebfe2e5dead6346fd143
Antonina Nesmelova <oslikna@gmail.com>
2022-11-30 21:43:53 +0100

    External links & footer (#86)
    * contact links
    
    * add footer
    
    Co-authored-by: Richard Jedlička <jedlicka.r@gmail.com>

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -1,24 +1,40 @@
 /** @jsxImportSource @emotion/react */
-import { useCallback, useEffect, useState } from "react";
-import { useSearchParams } from "react-router-dom";
+import { useState } from "react";
 import { css, Theme } from "@emotion/react";
 
 import { ReactComponent as Logo } from "../assets/calamar-logo-export-05.svg";
 import Background from "../assets/main-screen-bgr.svg";
 
 import { Link } from "../components/Link";
-import NetworkSelect from "../components/NetworkSelect";
 import SearchInput from "../components/SearchInput";
+import { Footer } from "../components/Footer";
 import { Devtool } from "../components/Devtool";
 
 const containerStyle = css`
 	width: 100vw;
 	height: 100vh;
+	margin: 0;
+	display: flex;
+	flex-direction: column;
+	align-items: stretch;
+`;
+
+const contentStyle = css`
+	position: relative;
+	flex: 1 1 auto;
+`;
+
+const backgroundStyle = css`
+	position: absolute;
+	bottom: 0;
+	margin: 0;
+	width: 100%;
+	height: 100vh;
 	background-position: center bottom;
 	background-size: contain;
 	background-repeat: no-repeat;
 	background-image: url(${Background});
-	margin: 0;
+	z-index: -1;
 `;
 
 const logoStyle = css`
@@ -28,7 +44,7 @@ const logoStyle = css`
 	max-width: 100%;
 `;
 
-const searchBoxStyle = (theme: Theme) => css`
+const searchBoxStyle = css`
 	display: flex;
 	margin: auto;
 	max-width: 1000px;
@@ -36,42 +52,6 @@ const searchBoxStyle = (theme: Theme) => css`
 	padding-right: 16px;
 	text-align: center;
 	justify-content: center;
-`;
-
-const networkSelectStyle = (theme: Theme) => css`
-	margin-bottom: 16px;
-	border-top-right-radius: 0;
-	border-bottom-right-radius: 0;
-
-	& .MuiInputBase-input {
-		//color: white;
-		//background-color: #61dafb;
-		font-size: 16px;
-		font-weight: 600;
-
-		&.MuiSelect-select {
-			padding: 16px 24px;
-		}
-	}
-
-	& .MuiOutlinedInput-notchedOutline,
-	&:hover .MuiOutlinedInput-notchedOutline,
-	&.Mui-focused .MuiOutlinedInput-notchedOutline {
-		//border-color: ${theme.palette.secondary.main};
-		border-color: #c4cdd5;
-		border-right: none;
-	}
-
-	&::after {
-		content: '';
-		display: block;
-		width: 1px;
-		height: 24px;
-		margin-left: -1px;
-		background-color: #c4cdd5;
-		position: relative;
-		z-index: 10;
-	}
 `;
 
 const searchInputStyle = (theme: Theme) => css`
@@ -92,24 +72,36 @@ const searchInputStyle = (theme: Theme) => css`
 	}
 `;
 
+const footerStyle = css`
+	background-color: #9af0f7;
+
+	> div {
+		max-width: 900px;
+	}
+`;
+
 function HomePage() {
 	const [network, setNetwork] = useState<string | undefined>();
 
 	return (
 		<div css={containerStyle}>
-			<Logo css={logoStyle} />
-			<div css={searchBoxStyle}>
-				<SearchInput
-					css={searchInputStyle}
-					defaultNetwork={"polkadot"}
-					onNetworkChange={setNetwork}
-					persistNetwork
-				/>
+			<div css={contentStyle}>
+				<div css={backgroundStyle} data-test="background" />
+				<Logo css={logoStyle} />
+				<div css={searchBoxStyle}>
+					<SearchInput
+						css={searchInputStyle}
+						defaultNetwork={"polkadot"}
+						onNetworkChange={setNetwork}
+						persistNetwork
+					/>
+				</div>
+				<div style={{ margin: "auto", width: "fit-content", marginTop: 24, textAlign: "center" }}>
+					<div><Link to={`/${network}/latest-extrinsics`}>Show latest extrinsics</Link></div>
+					<div><Devtool><Link to={`/${network}/runtime`}>Runtime specs</Link></Devtool></div>
+				</div>
 			</div>
-			<div style={{ margin: "auto", width: "fit-content", marginTop: 24, textAlign: "center" }}>
-				<div><Link to={`/${network}/latest-extrinsics`}>Show latest extrinsics</Link></div>
-				<div><Devtool><Link to={`/${network}/runtime`}>Runtime specs</Link></Devtool></div>
-			</div>
+			<Footer css={footerStyle} />
 		</div>
 	);
 }
