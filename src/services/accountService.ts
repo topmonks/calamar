5a377e8f5d161e64aec1920bdf81e860c1b8daa8
rostislavlitovkin <rostislavlitovkin@gmail.com>
2023-01-18 13:27:35 +0100

    getAccount optimised. removed useless packages

--- src/services/accountService.ts
+++ src/services/accountService.ts
@@ -1,8 +1,6 @@
 import { isAddress } from "@polkadot/util-crypto";
 import { decodeAddress } from "../utils/formatAddress";
 
-import { getExtrinsic } from "./extrinsicsService";
-
 export async function getAccount(network: string, address: string) {
 	if (!isAddress(address)) {
 		return null;
@@ -11,29 +9,9 @@ export async function getAccount(network: string, address: string) {
 	// if the address is encoded, decode it
 	const decodedAddress = decodeAddress(address);
 
-	if(decodedAddress.length !== 66) {
-		return null;
-	}
-
 	if (decodedAddress) {
 		address = decodedAddress;
 	}
 
-	const filter = {
-		OR: [
-			{ signature_jsonContains: `{"address": "${address}" }` },
-			{ signature_jsonContains: `{"address": { "value": "${address}"} }` },
-		],
-	};
-
-	const extrinsic = await getExtrinsic(network, filter);
-
-	if (!extrinsic) {
-		return { address: address, id: address };
-	}
-
-	return {
-		id: address,
-		address
-	};
+	return { address: address, id: address };
 }
