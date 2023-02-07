4c75cafcff108222f26fce301d8a46fe641c798b
Richard Jedlička <jedlicka.r@gmail.com>
2023-02-07 14:34:02 +0100

    fix call/event args parsing

--- src/utils/queryMetadata.ts
+++ src/utils/queryMetadata.ts
@@ -1,15 +1,15 @@
 import { DecodedMetadata } from "../model/decodedMetadata";
 
 export function getCallMetadataByName(metadata: DecodedMetadata, palletName: string, callName: string) {
-	const pallet = metadata.pallets.find(it => it.name.toLowerCase() === palletName);
-	const call = pallet?.calls.find(it => it.name.toLowerCase() === callName);
+	const pallet = metadata.pallets.find(it => it.name.toLowerCase() === palletName.toLowerCase());
+	const call = pallet?.calls.find(it => it.name.toLowerCase() === callName.toLowerCase());
 
 	return call;
 }
 
 export function getEventMetadataByName(metadata: DecodedMetadata, palletName: string, eventName: string) {
-	const pallet = metadata.pallets.find(it => it.name.toLowerCase() === palletName);
-	const event = pallet?.events.find(it => it.name.toLowerCase() === eventName);
+	const pallet = metadata.pallets.find(it => it.name.toLowerCase() === palletName.toLowerCase());
+	const event = pallet?.events.find(it => it.name.toLowerCase() === eventName.toLowerCase());
 
 	return event;
 }
