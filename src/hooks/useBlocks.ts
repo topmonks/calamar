7ae7831495cce663e7171ffe511addc882d51835
rostislavlitovkin <rostislavlitovkin@gmail.com>
2023-02-27 12:06:45 +0100

    fixed a mistype

--- src/hooks/useBlocks.ts
+++ src/hooks/useBlocks.ts
@@ -1,13 +1,12 @@
 import { FetchOptions } from "../model/fetchOptions";
-import { BlocksFilter, getBlocks } from "../services/blocksService";
-import { ExtrinsicsOrder } from "../services/extrinsicsService";
+import { BlocksFilter, BlocksOrder, getBlocks } from "../services/blocksService";
 
 import { usePaginatedResource } from "./usePaginatedResource";
 
 export function useBlocks(
 	network: string,
 	filter: BlocksFilter | undefined,
-	order?: ExtrinsicsOrder,
+	order?: BlocksOrder,
 	options?: FetchOptions
 ) {
 	return usePaginatedResource(getBlocks, [network, filter, order], options);
