d8ac296d26516bbea201ec1f52b92a14e3e825b6
Richard Jedlička <jedlicka.r@gmail.com>
2023-05-17 11:07:19 +0200

    account identity

---
+++ src/model/accountIdentity.ts
@@ -0,0 +1,9 @@
+export type AccountIdentity = {
+	id: string;
+	display: string|null;
+	legal: string|null;
+	web: string|null;
+	email: string|null;
+	twitter: string|null;
+	riot: string|null;
+}
--- src/services/accountService.ts
+++ src/services/accountService.ts
@@ -1,10 +1,14 @@
 import { isAddress } from "@polkadot/util-crypto";
 
 import { Account } from "../model/account";
+import { MainSquidIdentity } from "../model/main-squid/mainSquidIdentity";
 import { addRuntimeSpec } from "../utils/addRuntimeSpec";
 import { DataError } from "../utils/error";
+import { decodeAddress, encodeAddress } from "../utils/formatAddress";
 
-import { decodeAddress } from "../utils/formatAddress";
+import { getNetwork, hasSupport } from "./networksService";
+import { fetchMainIdentitiesSquid } from "./fetchService";
+import { AccountIdentity } from "../model/accountIdentity";
 
 export async function getAccount(network: string, address: string): Promise<Account|undefined> {
 	if (!isAddress(address)) {
@@ -18,12 +22,47 @@ export async function getAccount(network: string, address: string): Promise<Acco
 		address = decodedAddress;
 	}
 
-	const data = {
+	const data: Omit<Account, "runtimeSpec"> = {
 		id: address,
 		address,
+		identity: null
 	};
+
+	if (hasSupport(network, "main-identities-squid")) {
+		data.identity = await getAccountIdentity(network, address);
+	}
 
 	const account = await addRuntimeSpec(network, data, () => "latest");
 
 	return account;
+}
+
+/*** PRIVATE ***/
+
+async function getAccountIdentity(networkName: string, address: string) {
+	const network = getNetwork(networkName);
+
+	const response = await fetchMainIdentitiesSquid<{identity: MainSquidIdentity|null}>(
+		networkName,
+		`query ($id: String!) {
+			identity: identityById(id: $id) {
+				id
+				display
+				legal
+				web
+				email
+				twitter
+				riot
+			}
+		}`,
+		{
+			id: encodeAddress(address, network.prefix)
+		}
+	);
+
+	return response.identity && unifyMainSquidIdentity(response.identity);
+}
+
+function unifyMainSquidIdentity(identity: MainSquidIdentity): AccountIdentity {
+	return identity;
 }
