fc791d5ddc433cea71b3587dc0718c50a017c9ee
Richard Jedlička <jedlicka.r@gmail.com>
2023-05-19 09:55:03 +0200

    update networks

--- scripts/update-networks/config/squids.ts
+++ scripts/update-networks/config/squids.ts
@@ -4,7 +4,7 @@ export const squidUrlTemplates: Record<string, (network: string) => string> = {
 	balances: (network: string) => `https://squid.subsquid.io/${network}-balances/graphql`,
 	explorer: (network: string) => `https://squid.subsquid.io/gs-explorer-${network}/graphql`,
 	main: (network: string) => `https://squid.subsquid.io/gs-main-${network}/graphql`,
-	stats: (network: string) => `https://squid.subsquid.io/gs-stats-${network}/graphql`
+	//stats: (network: string) => `https://squid.subsquid.io/gs-stats-${network}/graphql`
 };
 
 export const forceSquidUrl: Record<string, Partial<Record<typeof squidTypes[number], string>>> = {
