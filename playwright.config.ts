fd803bc0eb5a834362fac765fb75ff6f5a42d0f8
Richard Jedlička <jedlicka.r@gmail.com>
2023-05-26 14:20:01 +0200

    do not run tests in parallel (timing out)

--- playwright.config.ts
+++ playwright.config.ts
@@ -27,8 +27,8 @@ const config: Config = {
 	forbidOnly: !!process.env.CI,
 	/* Retry on CI only */
 	retries: process.env.CI ? 2 : 0,
-	/* Opt out of parallel tests on CI. */
-	workers: process.env.CI ? 1 : undefined,
+	/* Opt out of parallel tests. */
+	workers: 1,
 	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
 	reporter: [
 		["html", {
